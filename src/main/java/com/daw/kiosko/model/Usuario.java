23753142201a67742ed96ec6c26052f4207df3da
Doradux <marcosloldorado@gmail.com>
2024-03-06 00:53:07 +0100

    funcionalidad guardar producto, vista create y logger para la clase ProductoController

--- src/main/java/com/daw/kiosko/model/Usuario.java
+++ src/main/java/com/daw/kiosko/model/Usuario.java
@@ -22,76 +22,100 @@ public class Usuario {
 	private String telefono;
 	private String tipo;
 	private String password;
-	
+
 	@OneToMany(mappedBy = "usuario")
 	private List<Producto> productos;
-	
+
 	@OneToMany(mappedBy = "usuario")
 	private List<Pedido> pedidos;
-	
+
 	public Usuario() {
 	}
-	
-	
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
+
+	public Usuario(int id, String nombre, String username, String email, String direccion, String telefono, String tipo,
+			String password) {
+		super();
 		this.id = id;
-	}
-	public String getNombre() {
-		return nombre;
-	}
-	public void setNombre(String nombre) {
 		this.nombre = nombre;
-	}
-	public String getUsername() {
-		return username;
-	}
-	public void setUsername(String username) {
 		this.username = username;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
 		this.email = email;
-	}
-	public String getDireccion() {
-		return direccion;
-	}
-	public void setDireccion(String direccion) {
 		this.direccion = direccion;
-	}
-	public String getTelefono() {
-		return telefono;
-	}
-	public void setTelefono(String telefono) {
 		this.telefono = telefono;
-	}
-	public String getTipo() {
-		return tipo;
-	}
-	public void setTipo(String tipo) {
 		this.tipo = tipo;
-	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
 		this.password = password;
 	}
 
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getDireccion() {
+		return direccion;
+	}
+
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+
+	public String getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
 
 	public List<Producto> getProductos() {
 		return productos;
 	}
 
-
 	public void setProductos(List<Producto> productos) {
 		this.productos = productos;
 	}
-
 
 	@Override
 	public String toString() {
@@ -100,7 +124,4 @@ public class Usuario {
 				+ "]";
 	}
 
-	
-	
-	
 }
