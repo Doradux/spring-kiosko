2f64454a4c6586f058c87e74a13a3cbb19fa8e0d
Doradux <marcosloldorado@gmail.com>
2024-03-05 22:20:35 +0100

    Creacion estructura proyecto y clases model Kiosko

---
+++ src/test/java/com/daw/kiosko/SpringKioskoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daw.kiosko;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringKioskoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
