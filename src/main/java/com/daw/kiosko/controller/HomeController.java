be15fdd41a610d6ebe78e8c28167290caa2711c4
Doradux <marcosloldorado@gmail.com>
2024-03-06 03:18:03 +0100

    mostrar vista producto a usuario

--- src/main/java/com/daw/kiosko/controller/HomeController.java
+++ src/main/java/com/daw/kiosko/controller/HomeController.java
@@ -1,9 +1,12 @@
 package com.daw.kiosko.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.daw.kiosko.service.ProductoService;
@@ -12,6 +15,8 @@ import com.daw.kiosko.service.ProductoService;
 @RequestMapping("/")
 public class HomeController {
 	
+	private final Logger LOGGER = LoggerFactory.getLogger(HomeController.class);
+	
 	@Autowired
 	private ProductoService productoService;
 	
@@ -19,5 +24,11 @@ public class HomeController {
 	public String home(Model model) {
 		model.addAttribute("productos", productoService.findAll());
 		return "usuario/home";
+	}
+	
+	@GetMapping("productohome/{id}")
+	public String productohome(@PathVariable Integer id, Model model) {
+		LOGGER.info("Id producto enviado como parametro {}", id);
+		return "usuario/productohome";
 	}
 }
